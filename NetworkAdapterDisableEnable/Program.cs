a1f116e087229c25cf4f66b74a5250d4d3e64b1a
Jacob Garner <Jacob.Garner@coxautoinc.com>
2020-06-25 22:04:15 -0600

    Add project files.

---
+++ NetworkAdapterDisableEnable/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace NetworkAdapterDisableEnable
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var adapter = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault(x => x.Name == "Wi-Fi");
+
+            if (adapter != null)
+            {
+                Console.WriteLine("Disabling Network Adapter");
+                Toggle(adapter.Name, InterfaceStatus.Disable);
+
+                Console.WriteLine("10 second timer...");
+                Thread.Sleep(10000);
+
+                Console.WriteLine("Enabling Network Adapter");
+                Toggle(adapter.Name, InterfaceStatus.Enable);
+
+                while (adapter.OperationalStatus != OperationalStatus.Up)
+                {
+                    Console.WriteLine($"Status: {adapter.OperationalStatus}");
+                    Thread.Sleep(500);
+                }
+
+                Console.WriteLine($"Operational Status: {adapter.OperationalStatus}");
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private static void Toggle(string interfaceName, InterfaceStatus interfaceStatus)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("netsh", $"interface set interface name={interfaceName} admin={interfaceStatus.ToString().ToUpper()}")
+            {
+                UseShellExecute = false, 
+                RedirectStandardOutput = true, 
+                RedirectStandardError = true
+            };
+
+            Process p = new Process {StartInfo = psi};
+            p.OutputDataReceived += (s, e) => Console.WriteLine(e.Data);
+            p.ErrorDataReceived += (s, e) => Console.WriteLine(e.Data);
+
+            p.Start();
+        }
+
+        private enum InterfaceStatus
+        {
+            Enable, Disable
+        }
+    }
+}
